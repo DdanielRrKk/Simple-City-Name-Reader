4e7f751169c2de2ef7d4dd0ba6ba3906e5371976
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2020-11-20 13:54:38 +0200

    Add files via upload

---
+++ Cities/Cities/City_Name.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cities
+{
+    public class City_Name
+    {
+        private string cities;
+
+        private static int count = 0;
+
+        public string getCities()
+        {
+            return cities;
+        }
+
+        
+
+        public string setBGName(string s)
+        {
+            bool errorFlag = false;
+
+            if (count < 3)
+            {
+                foreach (char c in s)
+                {
+                    if (c >= 'а' && c <= 'я' || c >= 'А' && c <= 'Я' || char.IsControl(c) || c == '-')
+                    {
+
+                    }
+                    else
+                    {
+                        errorFlag = true;
+                    }
+                }
+
+                if (errorFlag == false)
+                {
+                    cities = cities + " " + s;
+                    count++;
+                }
+            }
+
+            return cities;
+        }
+
+        public string setGBName(string s)
+        {
+            bool errorFlag = false;
+
+            if (count < 3)
+            {
+                foreach (char c in s)
+                {
+                    if (c >= 'а' && c <= 'я' || c >= 'А' && c <= 'Я')
+                    {
+                        errorFlag = true;
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+                if (errorFlag == false)
+                {
+                    cities = cities + " " + s;
+                    count++;
+                }
+            }
+
+            return cities;
+        }
+
+        public string uppercase()
+        {
+            string temp = "";
+
+            if (!string.IsNullOrEmpty(cities))
+            {
+                temp = cities.Substring(0, 1).ToUpper();
+
+                for (int i = 1; i < cities.Length; i++)
+                {
+                    if (cities.Substring(i - 1, 1) == " ")
+                    {
+                        temp = temp + cities.Substring(i, 1).ToUpper();
+                    }
+                    else
+                    {
+                        temp = temp + cities.Substring(i, 1);
+                    }
+                }
+
+            }
+
+            cities = temp;
+            return temp;
+        }
+
+        public string checkSpace()
+        {
+            string temp = "";
+
+            if (!string.IsNullOrEmpty(cities))
+            {
+                temp = cities.Substring(0, 1);
+
+                for (int i = 1; i < cities.Length; i++)
+                {
+                    if (cities.Substring(i - 1, 1) == " " && cities.Substring(i, 1) == " ")
+                    {
+                       
+                    }
+                    else
+                    {
+                        temp = temp + cities.Substring(i, 1);
+                    }
+                }
+
+            }
+
+            cities = temp;
+            return temp;
+        }
+
+        public string deleteLastCity()
+        {
+            string temp = "";
+
+            if (!string.IsNullOrEmpty(cities))
+            {
+                for(int i=0; i < cities.LastIndexOf(" "); i++)
+                {
+                    temp = temp + cities.Substring(i, 1);
+                }
+            }
+
+            cities = temp.Trim();
+            return temp;
+        }
+
+        public string deleteFirstCity()
+        {
+            string temp = "";
+
+            if (!string.IsNullOrEmpty(cities))
+            {
+                for (int i = cities.LastIndexOf(" "); i < cities.Length; i++)
+                {
+                    temp = temp + cities.Substring(i, 1);
+                }
+            }
+
+            cities = temp.Trim();
+            return temp;
+        }
+
+        public string removeFirstLines()
+        {
+            string temp = "";
+
+            if (!string.IsNullOrEmpty(cities))
+            {
+
+                string[] strArr = cities.Split(' ');
+                
+
+                foreach(string c in strArr)
+                {
+                    string temptemp = c;
+                    bool flag = false;
+
+                    while (flag == false)
+                    {
+                        if (temptemp.StartsWith("-"))
+                        {
+                            temptemp = temptemp.Substring(1);
+                        }
+                        else
+                        {
+                            flag = true;
+                        }
+                    }
+
+                    if (temp == "")
+                    {
+                        temp = temp + temptemp;
+                    }
+                    else
+                    {
+                        temp = temp + " " + temptemp;
+                    }
+                }
+
+            }
+
+            cities = temp;
+            return temp;
+        }
+
+        public string removeMiddleLines()
+        {
+            string temp = "";
+
+            if (!string.IsNullOrEmpty(cities))
+            {
+                temp = cities.Substring(0, 1);
+
+                for (int i = 1; i < cities.Length; i++)
+                {
+                    if (cities.Substring(i - 1, 1) == "-" && cities.Substring(i, 1) == "-")
+                    {
+
+                    }
+                    else
+                    {
+                        temp = temp + cities.Substring(i, 1);
+                    }
+                }
+
+            }
+
+            cities = temp;
+            return temp;
+        }
+
+        public string removeLastLines()
+        {
+            string temp = "";
+
+            if (!string.IsNullOrEmpty(cities))
+            {
+
+                string[] strArr = cities.Split(' ');
+
+
+                foreach (string c in strArr)
+                {
+                    string temptemp = c;
+                    bool flag = false;
+
+                    while (flag == false)
+                    {
+                        if (temptemp.EndsWith("-"))
+                        {
+                            int size = temptemp.Length;
+                            temptemp = temptemp.Substring(0, size - 1);
+                        }
+                        else
+                        {
+                            flag = true;
+                        }
+                    }
+
+                    if (temp == "")
+                    {
+                        temp = temp + temptemp;
+                    }
+                    else
+                    {
+                        temp = temp + " " + temptemp;
+                    }
+                }
+
+            }
+
+            cities = temp;
+            return temp;
+        }
+
+
+
+    }
+}
---
+++ dom8/dom8/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dom8
+{
+    public partial class Form1 : Form
+    {
+        Label lbl = new Label()
+        {
+            Text = "Тук ще бъдат имената на градовете",
+            Location = new Point(20, 20)
+        };
+
+        TextBox tb = new TextBox()
+        {
+            Location = new Point(20, 60)
+        };
+
+        Button btn=new Button()
+        {
+            Text="Прати Град",
+            Location = new Point(40, 100)
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Controls.Add(lbl);
+            this.Controls.Add(tb);
+            this.Controls.Add(btn);
+        }
+
+        public void btn_Click(Object sender, EventHandler e)
+        {
+
+        }
+
+    }
+}
